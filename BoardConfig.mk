8d84d1ab0da300ada81a5b2fbdbc17fbbd8d0fe4
Patrick Graber <pjgraber03@gmail.com>
2015-12-10 22:53:06 -0800

    Added bliss build options

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,3 +150,31 @@ BOARD_HARDWARE_CLASS := \
 USE_CLANG_PLATFORM_BUILD := true
 
 -include vendor/motorola/shamu/BoardConfigVendor.mk
+
+# Block_Build
+Bliss_Build_Block:=1
+
+# Blissful building options
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_GRAPHITE := true
+BLISS_STRICT := true
+BLISS_KRAIT := true
+BLISS_PIPE := true
+FLOOP_NEST_OPTIMIZE := true
+ENABLE_GCCONLY := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+FAST_MATH := true
+ENABLE_MODULAR_O3 := true
+ENABLE_LTO := true
+LINK_TIME_OPTIMIZATIONS := true
+TARGET_USE_ION_COMPAT := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_TC_ROM := 5.2-linaro
+TARGET_TC_KERNEL := 4.8-linaro
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+TARGET_ENABLE_UKM := true
+
+#WITH_LZMA_OTA := true
+#SaberMod
